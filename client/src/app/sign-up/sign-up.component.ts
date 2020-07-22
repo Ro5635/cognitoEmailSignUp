afff7a1a0dcfbd8fd2a704d9aa42e6096eba8609
Robert Curran <ro5635@gmail.com>
2020-07-22 17:02:32 +0100

    Implemented Sign-up page pull email from URL Params

--- client/src/app/sign-up/sign-up.component.ts
+++ client/src/app/sign-up/sign-up.component.ts
@@ -1,9 +1,9 @@
 // Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {Component, ChangeDetectionStrategy, OnInit} from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Router } from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import { AuthService } from '../auth.service';
 import { BehaviorSubject } from 'rxjs';
 
@@ -13,19 +13,36 @@ import { BehaviorSubject } from 'rxjs';
   styleUrls: ['./sign-up.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit  {
 
   email = new FormControl('');
+  public state;
 
   private busy_ = new BehaviorSubject(false);
   public busy = this.busy_.asObservable();
 
+  private shouldShowEmailCollectionForm_ = new BehaviorSubject(false);
+  public shouldShowEmailCollectionForm = this.busy_.asObservable();
+
   private errorMessage_ = new BehaviorSubject('');
   public errorMessage = this.errorMessage_.asObservable();
 
-  constructor(private router: Router, private auth: AuthService) { }
+    ngOnInit() {
+        if (this.email.value) {
+            this.signUp();
+        } else {
+            this.shouldShowEmailCollectionForm_.next(true);
+        }
+    }
 
-  public async signup() {
+  constructor(private router: Router, private auth: AuthService,  private route: ActivatedRoute) {
+      this.route.queryParams.subscribe(params => {
+          this.email.setValue(params['email'] ? params['email'] : '');
+          this.state = params['state'];
+      });
+  }
+
+  public async signUp() {
     this.errorMessage_.next('');
     this.busy_.next(true);
     try {
